9002025b20af049d3c6334b6ecb28867f2b1212f
ilyong <1yongs_@naver.com>
2023-07-07 17:48:51 +0900

    ADD repository (customer) memory implemented

--- internal/domain/customer/memory/memory.go
+++ internal/domain/customer/memory/memory.go
@@ -1,6 +1,8 @@
 package memory
 
 import (
+	"fmt"
+	"github.com/dlfdyd96/ddd-go/internal/domain/customer"
 	"sync"
 
 	"github.com/google/uuid"
@@ -22,16 +24,40 @@ func New() *Repository {
 }
 
 // Get finds a customer by ID
-func (r *Repository) Get(uuid.UUID) (aggregate.Customer, error) {
-	return aggregate.Customer{}, nil
+func (r *Repository) Get(id uuid.UUID) (aggregate.Customer, error) {
+	if customer, ok := r.customers[id]; ok {
+		return customer, nil
+	}
+	return aggregate.Customer{}, customer.ErrCustomerNotFound
 }
 
 // Add will add a new customer to the repository
-func (r *Repository) Add(aggregate.Customer) error {
+func (r *Repository) Add(c aggregate.Customer) error {
+	if r.customers == nil {
+		// Saftey check if customers is not create, shouldn't happen if using the Factory, but you never know
+		r.Lock()
+		r.customers = make(map[uuid.UUID]aggregate.Customer)
+		r.Unlock()
+	}
+
+	// Make sure Customer isn't already in the repository
+	if _, ok := r.customers[c.GetID()]; ok {
+		return fmt.Errorf("customer already exists: %w", customer.ErrFailedToAddCustomer)
+	}
+	r.Lock()
+	r.customers[c.GetID()] = c
+	r.Unlock()
 	return nil
 }
 
 // Update will replace an existing customer information with the new customer information
-func (r *Repository) Update(aggregate.Customer) error {
+func (r *Repository) Update(c aggregate.Customer) error {
+	// Make sure Customer is in the repository
+	if _, ok := r.customers[c.GetID()]; !ok {
+		return fmt.Errorf("customer does not exist: %w", customer.ErrUpdateCustomer)
+	}
+	r.Lock()
+	r.customers[c.GetID()] = c
+	r.Unlock()
 	return nil
 }
