92a0e5e62b86ac7ae4254b65b89f5262cdf9288a
ilyong <1yongs_@naver.com>
2023-07-07 17:11:14 +0900

    ADD customer(aggregate) factory

--- internal/aggregate/customer.go
+++ internal/aggregate/customer.go
@@ -1,8 +1,15 @@
 package aggregate
 
 import (
+	"errors"
 	"github.com/dlfdyd96/ddd-go/internal/entity"
 	"github.com/dlfdyd96/ddd-go/internal/valueobject"
+	"github.com/google/uuid"
+)
+
+var (
+	// ErrInvalidPerson is returned when the person is not valid in the NewCustomer factory
+	ErrInvalidPerson = errors.New("a customer has to have an valid person")
 )
 
 // Customer is a aggregate that combines all entities needed to represent a customer
@@ -15,4 +22,25 @@ type Customer struct {
 	// a customer can perform many transaction
 	// the value objects are held as nonpointers though since they cannot change state.
 	transactions []valueobject.Transaction
+}
+
+// NewCustomer is a factory to create a new Customer aggregate
+// It will validate that the name is not empty
+func NewCustomer(name string) (Customer, error) {
+	// Validate that the Name is not empty
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	// Create a new person and generate ID
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+	// Create a customer object and initialize all the values to avoid nil pointer exceptions
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
 }
