275ee9f0e14d127b4d40c874215cc2eee1773c51
ilyong <1yongs_@naver.com>
2023-07-07 17:53:45 +0900

    ADD repository (customer) memory test

--- internal/domain/customer/memory/memory.go
+++ internal/domain/customer/memory/memory.go
@@ -10,54 +10,54 @@ import (
 	"github.com/dlfdyd96/ddd-go/internal/aggregate"
 )
 
-// Repository fulfills the CustomerRepository interface
-type Repository struct {
+// MemoryRepository fulfills the CustomerRepository interface
+type MemoryRepository struct {
 	customers map[uuid.UUID]aggregate.Customer
 	sync.Mutex
 }
 
 // New is a factory function to generate a new repository of customers
-func New() *Repository {
-	return &Repository{
+func New() *MemoryRepository {
+	return &MemoryRepository{
 		customers: make(map[uuid.UUID]aggregate.Customer),
 	}
 }
 
 // Get finds a customer by ID
-func (r *Repository) Get(id uuid.UUID) (aggregate.Customer, error) {
-	if customer, ok := r.customers[id]; ok {
+func (mr *MemoryRepository) Get(id uuid.UUID) (aggregate.Customer, error) {
+	if customer, ok := mr.customers[id]; ok {
 		return customer, nil
 	}
 	return aggregate.Customer{}, customer.ErrCustomerNotFound
 }
 
 // Add will add a new customer to the repository
-func (r *Repository) Add(c aggregate.Customer) error {
-	if r.customers == nil {
+func (mr *MemoryRepository) Add(c aggregate.Customer) error {
+	if mr.customers == nil {
 		// Saftey check if customers is not create, shouldn't happen if using the Factory, but you never know
-		r.Lock()
-		r.customers = make(map[uuid.UUID]aggregate.Customer)
-		r.Unlock()
+		mr.Lock()
+		mr.customers = make(map[uuid.UUID]aggregate.Customer)
+		mr.Unlock()
 	}
 
 	// Make sure Customer isn't already in the repository
-	if _, ok := r.customers[c.GetID()]; ok {
+	if _, ok := mr.customers[c.GetID()]; ok {
 		return fmt.Errorf("customer already exists: %w", customer.ErrFailedToAddCustomer)
 	}
-	r.Lock()
-	r.customers[c.GetID()] = c
-	r.Unlock()
+	mr.Lock()
+	mr.customers[c.GetID()] = c
+	mr.Unlock()
 	return nil
 }
 
 // Update will replace an existing customer information with the new customer information
-func (r *Repository) Update(c aggregate.Customer) error {
+func (mr *MemoryRepository) Update(c aggregate.Customer) error {
 	// Make sure Customer is in the repository
-	if _, ok := r.customers[c.GetID()]; !ok {
+	if _, ok := mr.customers[c.GetID()]; !ok {
 		return fmt.Errorf("customer does not exist: %w", customer.ErrUpdateCustomer)
 	}
-	r.Lock()
-	r.customers[c.GetID()] = c
-	r.Unlock()
+	mr.Lock()
+	mr.customers[c.GetID()] = c
+	mr.Unlock()
 	return nil
 }
